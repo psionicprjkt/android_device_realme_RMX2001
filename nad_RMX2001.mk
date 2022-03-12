6c3dff7f5cfefba430736a2ddaca31feaf1d80d2
officialputuid <officialputuid@hack.id>
2022-03-12 13:34:25 +0800

    RMX2001: Squash makefile * * Update BUILD_FINGERPRINT & PRIVATE_BUILD_DESC, also change PRODUCT_MODEL
    Change-Id: I32e1ede6043c1d75bd6a1953475c5e18e6d69cfd

--- nad_RMX2001.mk
+++ nad_RMX2001.mk
@@ -25,14 +25,14 @@ $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 PRODUCT_NAME := nad_RMX2001
 PRODUCT_DEVICE := RMX2001
 PRODUCT_BRAND := realme
-PRODUCT_MODEL := Realme G90T Series
+PRODUCT_MODEL := realme 6
 PRODUCT_MANUFACTURER := realme
 
 # Build info
-BUILD_FINGERPRINT := "alps/sys_oplus_mssi_64_cn/oplus_mssi_64_cn:11/RP1A.200720.011/1607914664672:user/release-keys"
+BUILD_FINGERPRINT := "realme/RMX2001/RMX2001L1:10/QP1A.190711.020/1594211000:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=RMX2001 \
     PRODUCT_NAME=RMX2001 \
-    PRIVATE_BUILD_DESC="sys_oplus_mssi_64_cn-user 11 RP1A.200720.011 1607914664672 release-keys"
+    PRIVATE_BUILD_DESC="full_oppo6785-user 10 QP1A.190711.020 0b36b6c4f9b3bf3b release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
