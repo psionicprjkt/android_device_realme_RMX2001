0f8349d4482ce5c909f975bc6813c31d92b79d0d
SamarV-121 <samarvispute121@gmail.com>
2022-03-14 19:38:09 +0800

    wasabi: Build libsuspend and android.hardware.health@2.0
     * dependency of kpoc_charger
     * also remove useless libsysenv_system.so
    
    Change-Id: Ibdd34dfd492e2855e3594bad3e68f555ce2e64d5

--- device.mk
+++ device.mk
@@ -84,6 +84,11 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/idc/mtk-kpd.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/mtk-kpd.idc \
     $(DEVICE_PATH)/configs/keylayout/mtk-kpd.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/mtk-kpd.kl
 
+# KPOC
+PRODUCT_PACKAGES += \
+    libsuspend \
+    android.hardware.health@2.0
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.wasabi
