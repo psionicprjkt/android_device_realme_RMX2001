da72bc6c5a11888ec602f158a993eb5ccb966073
officialputuid <officialputuid@hack.id>
2022-03-14 20:17:27 +0800

    wasabi: Import mtk-ims permissions
    Change-Id: I57404e59c43f516df3db602ea1982377d760b8c6

--- device.mk
+++ device.mk
@@ -107,7 +107,8 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml \
+    $(DEVICE_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
 
 # Screen density
 PRODUCT_AAPT_CONFIG := xxxhdpi
