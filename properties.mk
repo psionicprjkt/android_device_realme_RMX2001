dbe7ba2a7bca605140339eed4004d006f3bcc540
Randall Huang <huangrandall@google.com>
2022-03-15 12:34:56 +0800

    wasabi: Enable ZRAM-writeback job
    This patch enables zram-writeback job. It will run idle marking
    at 60m since the device is boot. After that, it will do first
    writeback after 3hour to sweep initiallized cold pages out.
    After that, it periodically write out every 24hour.
    
    Bug: 117682284
    Bug: 127185055
    Test: confirm with manual testing
    Change-Id: I46ac496218142d60cc01fbb50dc95ce008c0157a

--- properties.mk
+++ properties.mk
@@ -74,3 +74,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     mediatek.wlan.ctia=0 \
     wifi.tethering.interface=ap0 \
     wifi.direct.interface=p2p0
+
+# ZRAM
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.zram.mark_idle_delay_mins=60 \
+    ro.zram.first_wb_delay_mins=180 \
+    ro.zram.periodic_wb_delay_hours=24
