ded778e0a8dd07501d8e522cdbfa3a9877ea927f
SamarV-121 <samarvispute121@gmail.com>
2022-03-16 11:29:16 +0800

    wasabi: Build init.recovery.mt6785.rc
    Change-Id: I42a442b572a86d1e0fb5b7b05599b959ebd793d1
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- device.mk
+++ device.mk
@@ -114,6 +114,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     FrameworkResOverlay
 
+# Recovery
+PRODUCT_PACKAGES += \
+    init.recovery.mt6785.rc
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml \
