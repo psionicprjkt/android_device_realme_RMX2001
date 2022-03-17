82457f78e28702229eec5617f00c86bcfc8be730
Midas Chien <midaschieh@google.com>
2022-03-17 17:12:33 +0800

    wasabi: props: Enable early SF configurations
    Sync early SF configurations from Bluecross
    
    Bug: 112620861
    Test: Launch Chrome animation test, frame drop rate 0.9%->0.24%
    Change-Id: Iac9b848ed86768ee99d4bbbb3b624d607b414005

--- properties.mk
+++ properties.mk
@@ -31,6 +31,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     sys.ipo.disable=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.early_app_phase_offset_ns=500000 \
+    debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000 \
+    debug.sf.early_phase_offset_ns=500000 \
     debug.sf.latch_unsignaled=1 \
     ro.surface_flinger.has_HDR_display=true \
     ro.surface_flinger.has_wide_color_display=true \
