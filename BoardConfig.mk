d149912eb2b5f5553be032675d7dfd22a3aaf72a
officialputuid <officialputuid@hack.id>
2022-03-18 12:25:40 +0800

    wasabi: Don't override BOARD_PLAT_PRIVATE_SEPOLICY_DIR
    Change-Id: Ia7f3408631c69d67e1be770422a7bd6225c0449f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -138,7 +138,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 # Sepolicy
 TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
 TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR := $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 # We have to skip checkpolicy because we have to re-define rild
 # in device system sepolicy to work around IMS issues.
 SELINUX_IGNORE_NEVERALLOWS := true
