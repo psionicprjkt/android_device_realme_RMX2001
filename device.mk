15aa06400e99086233fbd524010306e15e67f3ea
Anton Hansson <>
2022-08-20 16:19:14 +0800

    RMX2001: Remove PRODUCT_AAPT config
    This config strips unused drawable resources from the device
    to save space, but it is not particularly effective in practice
    (roughly ~30 MB across all partitions).
    
    Remove it to make the system image more generic.
    
    Bug: 118676832
    Test: make
    Change-Id: I3ae120773e793af6e7b5759d383b7fa9c65aeb48

--- device.mk
+++ device.mk
@@ -127,10 +127,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml \
     $(DEVICE_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
 
-# Screen density
-PRODUCT_AAPT_CONFIG := xxxhdpi
-PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES +=  \
     $(DEVICE_PATH)  \
