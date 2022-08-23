1ce6060d35b411b795c8ab436b8c8c9dcdf131d0
officialputuid <officialputuid@hack.id>
2022-08-23 15:56:29 +0800

    RMX2001: Drop privapp permissions whitelisting
    * Already in vendor/extra
    
    Change-Id: I71afaf781ec2574f63452684e90031dadb0bc9d9

--- properties.mk
+++ properties.mk
@@ -85,10 +85,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.mtk_perf_response_time=1 \
     ro.mtk_perf_simple_start_win=1
 
-# Privapp permissions whitelisting
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.control_privapp_permissions=log
-
 # RIL
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     vendor.rild.libargs=-d /dev/ttyC0 \
